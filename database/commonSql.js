d774dea35341c308a6a4527f83f3b8db522870d1
Denver Arancillo <denver.arancillo@gmail.com>
2025-03-16 21:22:49 +0800

    change library to sqlite3

--- database/commonSql.js
+++ database/commonSql.js
@@ -1,24 +1,38 @@
-import sqlite from 'aa-sqlite'
+// import sqlite from 'aa-sqlite'
+import sqlite3 from 'sqlite3'
+
+sqlite3.verbose()
 
 export default class commonSql {
-	static sql = sqlite
-	static async open() {
-		try {
-			await this.sql.open('./historylog.db')
-		} catch (error) {
-			console.error('common sql open connection', error)
-		}
+	static sql = sqlite3
+	static db
+
+	static open() {
+		return new Promise(resolve => {
+            this.db = new this.sql.Database('./historylog.db', error => {
+				if (error) reject("common sql open: " + error.message)
+				else {
+					this.db.run("PRAGMA foreign_keys=ON");
+					resolve(true)
+				}
+			});
+        });
 	}
 
-	static async close() {
-		await this.sql.close()
+	static close() {
+		return this.db.close()
 	}
 
-	static async all(query) {
-		try {
-			return await this.sql.all(query)
-		} catch (error) {
-			console.error('common sql all', error)
-		}
+	static async all(query, param = []) {
+		await this.open()
+
+		return new Promise((resolve, reject) => {
+			this.db.all(query, param, (error, rows) => {
+				if (error) return reject('common sql all: ' + error.message)
+
+				this.close()
+				resolve(rows)
+			})
+		})
 	}
 }
