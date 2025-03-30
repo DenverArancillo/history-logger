c148df2c36b0571985bb980804fe0504846322bc
Denver Arancillo <denver.arancillo@gmail.com>
2025-03-30 13:57:30 +0800

    add insert tag function and use new select common sql

--- database/tagQueries.js
+++ database/tagQueries.js
@@ -2,7 +2,7 @@ import commonSql from "./commonSql.js"
 
 const selectAllTags = async () => {
 	try {
-		return await commonSql.all('SELECT * FROM tag')
+		return await commonSql.selectAll('tag')
 	} catch (error) {
 		console.error('tagQueries select all tags', error)
 	}
@@ -10,13 +10,27 @@ const selectAllTags = async () => {
 
 const selectTagById = async id => {
 	try {
-		return await commonSql.get(`SELECT * FROM tag WHERE id = ${id}`)
+		return await commonSql.selectById('tag', id)
 	} catch (error) {
-		console.error('tagQueries select tag', error)		
+		console.error('tagQueries select tag', error)
+	}
+}
+
+ /**
+  * Insert new tag object to the database
+  * @param {object} newTag Information about the user
+  * @param {string} newTag.tag_name The name of the user
+  */
+const inserTag = async newTag => {
+	try {
+		await commonSql.insert('tag', newTag)
+	} catch (error) {
+		throw new Error('tagQueries insert tag' + error)
 	}
 }
 
 export {
 	selectAllTags,
-	selectTagById
+	selectTagById,
+	inserTag
 }
