da8bafd62e6a52590dad818c0d204e11c5506e9e
Denver Arancillo <denver.arancillo@gmail.com>
2025-04-07 20:30:11 +0800

    create base controller for history item

---
+++ controllers/historyItemController.js
@@ -0,0 +1,31 @@
+import {
+	selectQueryHistoryItems
+} from '../database/historyItemQueries.js'
+
+const getAllHistoryItems = async (req, res) => {
+	
+}
+
+const getHistoryItem = async (req, res, next) => {
+
+}
+
+const createHistoryItem = async (req, res, next) => {
+
+}
+
+const updateHistoryItem = async (req, res, next) => {
+
+}
+
+const deleteHistoryItem = async (req, res, next) => {
+
+}
+
+export {
+	getAllHistoryItems,
+	getHistoryItem,
+	createHistoryItem,
+	updateHistoryItem,
+	deleteHistoryItem
+}
