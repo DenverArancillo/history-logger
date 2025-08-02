0b288ed7170a84e3d551ae735cc360872ecf2861
Denver Arancillo <denver.arancillo@gmail.com>
2025-08-02 15:59:07 +0800

    rewrite error.js middleware to typescript and apple types

--- middleware/error.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-	if (error.status) {
-		res.status(error.status).json({ message: error.message })
-	} else {
-		res.status(500).json({ message: error.message })
-	}
-}
-
-export default errorHandler
