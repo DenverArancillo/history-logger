8c1e1be41464a1da8068c505bb339e45f080eb01
Denver Arancillo <denver.arancillo@gmail.com>
2025-09-09 22:26:25 +0800

    create tags api test spec file

--- tests/playwright/tests/api/tags.spec.ts
+++ tests/playwright/tests/api/tags.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect, type APIResponse } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+import { Tag } from '../../../../ts/interface/database/tags'
+import { faker } from '@faker-js/faker'
 
 test.describe.configure({ mode: 'serial' });
 
@@ -6,19 +8,19 @@ test.describe('Tags', () => {
 	let test_tag_id: number
 
 	test('Get all tags', async ({ request }) => {
-		let tags: APIResponse = await request.get('/api/tags')
-		let tagsData = await tags.json()
+		const response = await request.get('/api/tags')
+		let tagsData: Tag[] = await response.json()
 		
-		expect(tags.status()).toEqual(200)
+		expect(response.status()).toEqual(200)
 		expect(tagsData.length >= 0).toBeTruthy()
 		expect(Array.isArray(tagsData)).toBeTruthy()
 	})
 
 	test('Get single tag', async ({ request }) => {
-		let tag: APIResponse = await request.get('/api/tags/1')
-		let tagsData = await tag.json()
+		const response = await request.get('/api/tags/1')
+		let tagsData: Tag = await response.json()
 
-		expect(tag.status()).toEqual(200)
+		expect(response.status()).toEqual(200)
 		expect(tagsData).toHaveProperty('id')
 		expect(tagsData).toHaveProperty('tag_name')
 		expect(
@@ -29,12 +31,80 @@ test.describe('Tags', () => {
 	})
 
 	test('Create tag', async ({ request }) => {
-		console.log('test_tag_id', test_tag_id)
+		let tag_name = `createTagName-${faker.word.adjective()}-${new Date().toLocaleString('en-PH', {timeZone: 'Asia/Manila'})}`
+		const response = await request.post('/api/tags/', {
+			data: { tag_name }
+		})
 
-		test_tag_id = 1
+		let tags: Tag[] = await response.json()
+		let createdTag = tags.find(({ tag_name: name }) => name === tag_name)
+		if (createdTag && createdTag.id) {
+			test_tag_id = createdTag?.id
+		} else {
+			throw new Error('test data tag is undefined')
+		}
+
+		expect(response.status()).toEqual(201)
+		expect(tags.length >= 0).toBeTruthy()
+		expect(Array.isArray(tags)).toBeTruthy()
+		
+		for (let tag of tags) {
+			expect(tag).toHaveProperty('id')
+			expect(tag).toHaveProperty('tag_name')
+		}
 	})
 
-	test('next test', async ({ request }) => {
-		console.log('test_tag_id', test_tag_id)
+	test('POST error missing tag_name', async ({ request }) => {
+		const response = await request.post('/api/tags/')
+
+		let errorMessage = await response.json()
+
+		expect(response.status()).toEqual(400)
+		expect(errorMessage).toHaveProperty('message')
+	})
+
+	test('Update tag', async ({ request }) => {
+		let tag_name = `updateTag-${faker.word.adjective()}-${new Date().toLocaleString('en-PH', {timeZone: 'Asia/Manila'})}`
+		const response = await request.put(`/api/tags/${test_tag_id}`, {
+			data: { tag_name }
+		})
+
+		let tagsData: Tag = await response.json()
+
+		expect(response.status()).toEqual(200)
+		expect(tagsData).toHaveProperty('id')
+		expect(tagsData).toHaveProperty('tag_name')
+		expect(
+			typeof tagsData === 'object' && 
+			!Array.isArray(tagsData) && 
+			tagsData !== null
+		).toBeTruthy()
+	})
+
+	test('PUT error missing tag_name', async ({ request }) => {
+		const response = await request.post(`/api/tags/${test_tag_id}`)
+
+		let errorMessage = await response.json()
+
+		expect(response.status()).toEqual(404)
+		expect(errorMessage).toHaveProperty('message')
+	})
+
+	test('Delete tag', async ({ request }) => {
+		const response = await request.delete(`/api/tags/${test_tag_id}`)
+		
+		expect(response.status()).toEqual(200)
+		expect(await response.json()).toHaveProperty('message')
+
+		const getResponse = await request.get(`/api/tags/${test_tag_id}`)
+		expect(getResponse.status()).toEqual(404)
+	})
+
+	test('DELETE error missing tag id', async ({ request }) => {
+		const response = await request.delete('/api/tags/')
+		let errorMessage = await response.json()
+
+		expect(response.status()).toEqual(404)
+		expect(errorMessage).toHaveProperty('message')
 	})
 })
